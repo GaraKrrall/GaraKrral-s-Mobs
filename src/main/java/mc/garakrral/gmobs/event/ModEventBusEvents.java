7fd50b716314598b64486589f4b9187f90b61711
GaraKrrall <akglsms@gmail.com>
2025-11-16 23:24:41 +0300

    a

--- src/main/java/mc/garakrral/gmobs/event/ModEventBusEvents.java
+++ src/main/java/mc/garakrral/gmobs/event/ModEventBusEvents.java
@@ -1,15 +1,22 @@
 package mc.garakrral.gmobs.event;
 
 import mc.garakrral.gmobs.Main;
+import mc.garakrral.gmobs.entity.ModEntities;
+import mc.garakrral.gmobs.entity.client.model.GeckoModel;
 
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 
 @EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(GeckoModel.LAYER_LOCATION, GeckoModel::createBodyLayer);
+    }
 
+    private static void registerAttributes(EntityAttributeCreationEvent event) {
+        event.put(ModEntities.GECKO.get());
     }
 }
